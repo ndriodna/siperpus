deb5960560180ff3c11e96848b40e586f1c8d234
eeQ <kurodesu6969@gmail.com>
2021-12-13 22:12:26 +0800

    add migration & install breeze

---
+++ database/migrations/2021_12_13_131328_create_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')
+            ->onDelete('cascade');;
+            $table->integer("nim");
+            $table->string("nama");
+            $table->enum("jk",['L','P']);
+            $table->string("jurusan");
+            $table->string("telp")->nullable();
+            $table->string("alamat")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
