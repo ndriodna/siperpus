f06be8f96647cb061a0ba4c4cf58dfdb322a38e8
eeQ <kurodesu6969@gmail.com>
2021-12-18 20:30:51 +0800

    petugas & member nda paham

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Member;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
@@ -14,7 +15,9 @@ class MemberController extends Controller
      */
     public function index()
     {
-        return view('dashboard.member.index');
+        $members = Member::with('user')->paginate(10);
+        $users = User::get();
+        return view('dashboard.member.index',compact('members','users'));
     }
 
     /**
