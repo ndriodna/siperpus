c1bfc5e1ad1cbfe9c30cc466673f6d77cefb7e23
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-01-25 20:00:49 +0800

    conflict v4

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -221,26 +221,7 @@
                         </div>
                     </div>
                 </div>
-            @else
-                <div class="card shadow-lg">
-                    <div class="card-body">
-                        <div class="flex justify-between">
-                            <div><span class="font-semibold text-gray-500">Peminjaman </span><br> <span
-                                    class="text-xl font-semibold">{{ $transakiAuthMember->count() }}</span></div>
-                            <div class="flex justify-end">
-                                <button class="btn btn-error btn-md btn-circle">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                        viewBox="0 0 24 24" stroke="white">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             @endif
         </div>
     @endif
-    </div>
 </x-app-layout>
